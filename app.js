63db8ca3b584f5ce61d09207f5849158cb4bc90c
akash2003git <akash.tayade.dev@gmail.com>
2024-11-24 22:16:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const inputForm = document.querySelector("form");
+const searchQuery = document.querySelector("#searchBar");
+const outputBox = document.querySelector(".wrapper");
+
+inputForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  outputBox.innerHTML = "";
+  const searchTerm = searchQuery.value;
+  const res = await getTvShow(searchTerm);
+  makeImages(res.data);
+  searchQuery.value = "";
+});
+
+const getTvShow = async (searchTerm) => {
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  return res;
+};
+
+const makeImages = (data) => {
+  for (let item of data) {
+    if (item.show.image) {
+      const img = document.createElement("img");
+      img.src = item.show.image.original;
+      outputBox.append(img);
+    }
+  }
+};
